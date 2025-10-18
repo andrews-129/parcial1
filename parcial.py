b97a064952c38ad052dea521acdc33c5069e893d
andrews-129 <andresdavidrivera753@gmail.com>
2025-10-18 13:15:34 -0500

    Enhance phrase and perfect number programs
    Added output for the length of the longest word and corrected the perfect number check output.

--- parcial.py
+++ parcial.py
@@ -10,8 +10,8 @@ for palabra in palabras:
         palabra_larga = palabra
         longitud = len(palabra)
 
-print("La palabra más larga es:", palabra_larga)
-
+print("La palabra más larga es: ", palabra_larga)
+print("La longitud es: ", longitud) 
 #Crea un programa que determine si un número ingresado por el usuario es un número perfecto (la suma de sus divisores propios es igual al número).
 num = int(input("Ingrese el numero: "))
 sum_div = 0
@@ -22,4 +22,5 @@ for i in range (1, num):
 if sum_div == num:
     print("El numero ", str(num), " es perfecto")
 else:
+
     print("El numero ", str(num), " no es perfecto")
